c6893167665fbeb800ec051bebb0a066913ca029
Ewen Lukacik <newelukacik7360@gmail.com>
2026-03-22 13:11:56 +0100

    Feat: requiring the user to be connected to use some features

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import movieList, { create, destroy, show, update } from "../Controller/MovieController.js";
 import { requireAdminQuery } from "../Middlewares/requireAdminQuery.js";
+import { requireAuth } from "../Middlewares/requireAuth.js";
 import { movieValidator } from "../Middlewares/validateMovie.js";
 
 
@@ -9,8 +10,8 @@ const moviesRouter = Router();
 //Movie related routes
 moviesRouter.get('/', movieList);
 moviesRouter.get('/:id', show);
-moviesRouter.post('/', movieValidator, create);
-moviesRouter.put('/:id', movieValidator, update);
-moviesRouter.delete('/:id', requireAdminQuery, destroy);
+moviesRouter.post('/', requireAuth, movieValidator, create);
+moviesRouter.put('/:id', requireAuth, movieValidator, update);
+moviesRouter.delete('/:id', requireAuth, requireAdminQuery, destroy);
 
 export default moviesRouter;
