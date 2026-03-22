d3d4a4f5c701e150850724bc713b45d4d4222408
Ewen Lukacik <newelukacik7360@gmail.com>
2026-03-22 11:48:45 +0100

    feat: added validation methods

--- src/Middlewares/userValidator.js
+++ src/Middlewares/userValidator.js
@@ -1,6 +1,41 @@
+import bcrypt from "bcrypt";
 import { body } from "express-validator";
+import database from "../database.js";
 import handleValidationErrors from "./handleValidationErrors.js";
 
+export async function checkEmailNotTaken(req, res, next) {
+ try {
+    const { email } = req.body;
+
+    const [users] = await database.query(
+"SELECT id FROM users WHERE email = ?", [email]);
+
+    if (users.length > 0) {
+      return res.status(409).json({ error: "Email already used" });
+    }
+
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+}
+
+
+export async function hashPassword(req, res, next){
+    try{
+        const { password } = req.body
+
+        req.body.hashedPassword = await bcrypt.hash(password, 10);
+
+        delete req.body.password;
+
+        next();
+    } catch (err) {
+        console.error(err),
+        res.sendStatus(500);
+    }
+}
 export const validateUser = [
     body('name')
         .notEmpty().withMessage('Username required')
@@ -10,7 +45,10 @@ export const validateUser = [
         .isEmail().withMessage("Invalid email format"),
     body("password")
         .notEmpty().withMessage("Password required")
-        .isLength({ min: 10 }).withMessage("10 characters minimum"),
+        .isLength({ min: 10 }).withMessage("10 characters minimum")
+        .matches(/[A-Z]/).withMessage("Password must have at least one uppercase letter")
+        .matches(/[0-9]/).withMessage("Password must have at least one number"),
 
     handleValidationErrors
 ];
+
