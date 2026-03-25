6204cb7c4012c6545695539e737053163e9f0f71
Ewen Lukacik <newelukacik7360@gmail.com>
2026-03-25 20:08:35 +0100

    feat: made the movie card a link to its dedicated page

--- web/src/components/MovieCard.jsx
+++ web/src/components/MovieCard.jsx
@@ -1,30 +1,34 @@
 import { useMoviePoster } from "../hooks/useMoviePoster";
 import styles from './MovieCard.module.css';
+import { Link } from "react-router-dom";
 
 export default function MovieCard({ movie }){
     const posterUrl = useMoviePoster(movie.title, movie.release_year);
 
     return(
-        <article className={styles.card}>
-            <div className={styles.poster}>
-                {posterUrl ?
-                    (
-                        <img src={posterUrl} alt={movie.title} />
-                    ) : 
-                    (
-                        <div className={styles.noPoster}>No poster</div>
-                    )
-                }
-            </div>
-            <div className={styles.info}>
-                <div className={styles.meta}>
-                    <span className={styles.rating}> {movie.rating}/10 </span>
-                    <span className={styles.year}> {movie.release_year} </span>
+        <Link to={`/movies/${movie.id}`} className={styles.link}>
+            <article className={styles.card}>
+                <div className={styles.poster}>
+                    {posterUrl ?
+                        (
+                            <img src={posterUrl} alt={movie.title} />
+                        ) : 
+                        (
+                            <div className={styles.noPoster}>No poster</div>
+                        )
+                    }
                 </div>
-                <h2 className={styles.title}> {movie.title} </h2>
-                <p className={styles.director}> {movie.director} </p>
-                <span className={styles.category}> {movie.name} </span>
-            </div>
-        </article>
+                <div className={styles.info}>
+                    <div className={styles.meta}>
+                        <span className={styles.rating}> {movie.rating}/10 </span>
+                        <span className={styles.year}> {movie.release_year} </span>
+                    </div>
+                    <h2 className={styles.title}> {movie.title} </h2>
+                    <p className={styles.director}> {movie.director} </p>
+                    <span className={styles.category}> {movie.name} </span>
+                </div>
+            </article>
+        </Link>
+       
     )
 }
