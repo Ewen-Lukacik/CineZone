7d3f796ee11f5296de41bc726670019c9794ca88
Ewen Lukacik <newelukacik7360@gmail.com>
2026-03-26 15:13:48 +0100

    feat: created services and hooks for watcglist and favorites

---
+++ web/src/services/favoriteService.js
@@ -0,0 +1,47 @@
+const BASE_URL = '/api';
+
+export async function getFavorites(token){
+    const response = await fetch(`${BASE_URL}/favorites`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+
+    if(!response.ok){
+        throw new Error('failed to fetch favorites');
+    }
+
+    return response.json();
+}
+
+export async function addFavorites(token, movie_id){
+    const response = await fetch(`${BASE_URL}/favorites`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ movie_id })
+    });
+
+    if(!response.ok && response.status !== 409){
+        throw new Error('failed to add to favories');
+    }
+
+    return response.json();
+}
+
+export async function removeFavorites(token, movie_id){
+    const response = await fetch(`${BASE_URL}/favorite/${movie_id}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+
+    if(!response.ok){
+        throw new Error('failed to remove favories');
+    }
+
+    return response.json();
+}
